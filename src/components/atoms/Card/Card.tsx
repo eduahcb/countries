919cadced869aea62c60bbffd4df3aa828701b34
eduahcb <dev_eduardo@hotmail.com>
2023-02-28 22:34:22 -0300

    feat: creating Card component

---
+++ src/components/atoms/Card/Card.tsx
@@ -0,0 +1,24 @@
+import { isFunction } from 'helpers/isFunction'
+import React, { ReactElement, ReactNode, HTMLAttributes, SyntheticEvent } from 'react'
+
+import { Root } from './style'
+
+export type CardProps = HTMLAttributes<HTMLDivElement> & {
+  children: ReactNode
+  clickable?: boolean
+  onClick?: (event: SyntheticEvent) => void
+}
+
+export const Card = ({ children, clickable, onClick: onClickProp, ...rest }: CardProps): ReactElement => {
+  const handleOnClick = (event: SyntheticEvent): void => {
+    onClickProp && isFunction(onClickProp) && onClickProp(event)
+  }
+
+  return clickable
+    ? (
+        <Root clickable role="button" onClick={handleOnClick} {...rest}>{ children }</Root>
+      )
+    : (
+        <Root {...rest} onClick={handleOnClick}>{ children }</Root>
+      )
+}
